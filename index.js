dc3f07b034911612639bf41dfce842cfd9a497ff
kelleynurena@gmail.com Urena <kelleynurena@gmail.com>
2018-08-19 22:18:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  delete object.key
-  return object
+  const newObject = {...object}
+  delete newObject.key
+  return newObject
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
